015d4772f9490bd1da3ed71902499734057728ef
jonathan-cederquist <97691358+jonathan-cederquist@users.noreply.github.com>
2022-02-10 11:21:29 -0800

    Closer but Still, no cigar

--- src/main.py
+++ src/main.py
@@ -25,74 +25,35 @@ def task1_Motor ():
     Task which drivers the motor.
     """
     
-    S0_INIT = 0
-    S1_RUNNING = 1
-    S2_STOPPED = 2
+    #Initialize and Enable Motor 1
+    Slo_Moe = MotorDriver.MotorDriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, pyb.Pin.board.PB5, 3)
     
-    state = S0_INIT
+    Slo_Moe.enable()
+    
     while True:
-        
-        if state == S0_INIT:
-            Slo_Moe = MotorDriver.MotorDriver(pyb.Pin.board.PA10, pyb.Pin.board.PB4, pyb.Pin.board.PB5, 3)
-    
-            Slo_Moe.enable()
-            
-            state = S1_RUNNING
-            
-        elif state == S1_RUNNING:
-            Slo_Moe.set_duty_cycle(share_duty_1.get())
-            
-            if share_Stop.get() == 1:
-                state = S2_STOPPED
+        Slo_Moe.set_duty_cycle(share_duty_1.get())
                 
-        elif state == S2_STOPPED:
-            Slo_Moe.set_duty_cycle(0)
-            Slo_Moe.disable()
-            print("IN STOP STATE")
-        
-            if not share_Stop.get():
-                state = S0_INIT
-                
-        yield (state)
+        yield (0)
 
 def task2_Encoder ():
     """!
     Task which runs the encoder.
     """
-    S0_INIT = 0
-    S1_RUNNING = 1
-    S2_STOPPED = 2
+
+    # Initialize and zero Encoder 1
+    Slo_Enco = EncoderDriver.EncoderDriver(pyb.Pin(pyb.Pin.cpu.B6), pyb.Pin(pyb.Pin.cpu.B7), 4)
     
-    state = S0_INIT
+    Slo_Enco.zero()
+    
     while True:
-        
-        if state == S0_INIT:
-            Slo_Enco = EncoderDriver.EncoderDriver(pyb.Pin(pyb.Pin.cpu.B6), pyb.Pin(pyb.Pin.cpu.B7), 4)
     
-            Slo_Enco.zero()
-            
-            state = S1_RUNNING
-            
-        elif state == S1_RUNNING:
-            Slo_Enco.update()
-            share_pos_1.put(Slo_Enco.read())
-            
-            if not(queue_enc1Times.full()):
-                queue_enc1Times.put(utime.ticks_diff(utime.ticks_ms(), share_StartTime.get()))
-            
-            if not(queue_pos_1.full()):
-                queue_pos_1.put(Slo_Enco.read())
-            
-            if share_Stop.get():
-                state = S2_STOPPED
-                
-        elif state == S2_STOPPED:
-            Slo_Enco.zero()
+        Slo_Enco.update()
+        share_pos_1.put(Slo_Enco.read())
         
-            if not(share_Stop.get()):
-                state = S0_INIT
-                
-        yield (state)
+        if share_Stop.get():
+            Slo_Enco.zero()
+            
+        yield (0)
     
 def task3_Controller ():
     """!
@@ -103,21 +64,38 @@ def task3_Controller ():
     S2_STOPPED = 2
     
     state = S0_INIT
+    
+    Slo_Control = ClosedLoop.ClosedLoop(50, share_setpoint_1.get())
+    
     while True:
         
         if state == S0_INIT:
-            Slo_Control = ClosedLoop.ClosedLoop(50, share_setpoint_1.get())
+            Slo_Control.change_setpoint(share_setpoint_1.get())
             
-            state = S1_RUNNING
+            if share_Start.get():
+                share_Start.put(0)
+                share_pos_1.put(0)
+                state = S1_RUNNING
             
         elif state == S1_RUNNING:
+            
+            # Update control signal and duty cycle
             share_duty_1.put(int(Slo_Control.update(share_pos_1.get())))
             
+            # Save time and position data
+            if not queue_enc1Times.full():
+                queue_enc1Times.put(utime.ticks_diff(utime.ticks_ms(), share_StartTime.get()))
+            
+            if not queue_pos_1.full():
+                queue_pos_1.put(share_pos_1.get())
+                
             if share_Stop.get():
                 state = S2_STOPPED
                 
         elif state == S2_STOPPED:
-            Slo_Control.change_kp(0)
+
+            share_duty_1.put(0)
+            
         
             if not share_Stop.get():
                 state = S0_INIT
@@ -128,6 +106,7 @@ def task4_Motor2 ():
     """!
     Task which drivers the motor.
     """
+    #Initialize and Enable Motor 1        
     Bro_Moe = MotorDriver.MotorDriver(pyb.Pin.board.PC1, pyb.Pin.board.PA0, pyb.Pin.board.PA1, 5)
     
     Bro_Moe.enable()
@@ -135,7 +114,6 @@ def task4_Motor2 ():
     while True:
         
         Bro_Moe.set_duty_cycle(share_duty_2.get())
-        #print('Duty cycle: ', share_duty_2.get())
 
         yield (0)
 
@@ -143,40 +121,73 @@ def task5_Encoder2 ():
     """!
     Task which runs the encoder.
     """
+    # Initialize and zero Encoder 2
     Bro_Enco = EncoderDriver.EncoderDriver(pyb.Pin(pyb.Pin.cpu.C6), pyb.Pin(pyb.Pin.cpu.C7), 8)
     
     Bro_Enco.zero()
     
-    counter = 0
-    
     while True:
-        
+    
         Bro_Enco.update()
         share_pos_2.put(Bro_Enco.read())
         
-        if not(queue_pos_2.full()):
-            queue_pos_2.put(Bro_Enco.read())
-        #print('Encoder Position: ', share_pos_2.get())
-
-        yield (counter)
-        counter += 1
-
+        if share_Stop_2.get():
+            Bro_Enco.zero()
+            
+        yield (0)
+        
 def task6_Controller2 ():
     """!
     Task which runs the closed loop controller.
     """
+    S0_INIT = 0
+    S1_RUNNING = 1
+    S2_STOPPED = 2
     
-    Bro_Control = ClosedLoop.ClosedLoop(100, math.pi*2)
+    state = S0_INIT
+    
+    Bro_Control = ClosedLoop.ClosedLoop(50, share_setpoint_2.get())
     
     while True:
         
-        Bro_Control.change_setpoint(share_setpoint_2.get())
+        if state == S0_INIT:
+            Bro_Control.change_setpoint(share_setpoint_2.get())
+            
+            if share_Start_2.get():
+                share_Start_2.put(0)
+                share_pos_2.put(0)
+                state = S1_RUNNING
+            
+        elif state == S1_RUNNING:
+            
+            # Update control signal and duty cycle
+            share_duty_2.put(int(Bro_Control.update(share_pos_2.get())))
+            
+            # Save time and position data
+            if not queue_enc2Times.full():
+                queue_enc2Times.put(utime.ticks_diff(utime.ticks_ms(), share_StartTime.get()))
+            
+            if not queue_pos_2.full():
+                queue_pos_2.put(share_pos_2.get())
+                
+            if share_Stop_2.get():
+                state = S2_STOPPED
+                
+        elif state == S2_STOPPED:
+
+            share_duty_2.put(0)
+            
         
-        #print('Control Setpoint: ', share_setpoint_2.get())
+            if not share_Stop_2.get():
+                state = S0_INIT
+                
+        yield (state)
         
-        share_duty_2.put(int(Bro_Control.update(share_pos_2.get())))
-        
-        yield (0)
+def task7_User ():
+    """!
+    Task which interacts with the user.
+    """
+    
         
 # This code creates a share, a queue, and two tasks, then starts the tasks. The
 # tasks run until somebody presses ENTER, at which time the scheduler stops and
@@ -187,94 +198,110 @@ if __name__ == "__main__":
     share_duty_1 = task_share.Share ('i', thread_protect = False, name = "share_duty_1")
     share_setpoint_1 = task_share.Share ('f', thread_protect = False, name = "share_setpoint_1")
     share_pos_1 = task_share.Share ('f', thread_protect = False, name = "share_pos_1")
-    queue_pos_1 = task_share.Queue('f', 500, thread_protect = False, name = "queue_pos_1")
-    queue_enc1Times = task_share.Queue('f', 500, thread_protect = False, name = "queue_enc1Times")
-    share_StartTime = task_share.Share('i', thread_protect = False, name = "share_StartTime")
-    share_Stop = task_share.Share('i', thread_protect = False, name = "share_Stop")
+    queue_pos_1 = task_share.Queue('f', 100, thread_protect = False, name = "queue_pos_1")
+    queue_enc1Times = task_share.Queue('f', 100, thread_protect = False, name = "queue_enc1Times")
     
     # Create a shares for second motor duty cycle, controller setpoint, and encoder position
     share_duty_2 = task_share.Share ('i', thread_protect = False, name = "share_duty_2")
     share_setpoint_2 = task_share.Share ('f', thread_protect = False, name = "share_setpoint_2")
     share_pos_2 = task_share.Share ('f', thread_protect = False, name = "share_pos_2")
-    queue_pos_2 = task_share.Queue('f', 200, thread_protect = False, name = "queue_pos_2")
+    queue_pos_2 = task_share.Queue('f', 100, thread_protect = False, name = "queue_pos_2")
+    queue_enc2Times = task_share.Queue('f', 100, thread_protect = False, name = "queue_enc2Times")
+    
+    # Create share flags to control states in tasks
+    share_StartTime = task_share.Share('i', thread_protect = False, name = "share_StartTime")
+    share_Start = task_share.Share('i', thread_protect = False, name = "share_Start")
+    share_Stop = task_share.Share('i', thread_protect = False, name = "share_Stop")
+    share_Start_2 = task_share.Share('i', thread_protect = False, name = "share_Start_2")
+    share_Stop_2 = task_share.Share('i', thread_protect = False, name = "share_Stop_2")
     
     # Initialize shared variables
     share_duty_1.put(0)
     share_setpoint_1.put(math.pi*2)
     share_pos_1.put(0)
     share_duty_2.put(0)
-    share_setpoint_2.put(math.pi*2)
+    share_setpoint_2.put(math.pi*4)
     share_pos_2.put(0)
+    share_Stop.put(0)
+    share_Stop_2.put(0)
     
     # First set of tasks
     task1_Mot = cotask.Task (task1_Motor, name = 'Task1_Motor', priority = 1, 
-                         period = 10, profile = True, trace = False)
+                         period = 20, profile = True, trace = False)
     task2_Enco = cotask.Task (task2_Encoder, name = 'Task2_Encoder', priority = 3, 
                          period = 10, profile = True, trace = False)
     task3_Cont = cotask.Task (task3_Controller, name = 'Task3_Controller', priority = 2, 
-                         period = 10, profile = True, trace = False)
+                         period = 20, profile = True, trace = False)
             
     # Second set of tasks
     task4_Mot2 = cotask.Task (task4_Motor2, name = 'Task4_Motor2', priority = 1, 
-                         period = 10, profile = True, trace = False)
+                         period = 20, profile = True, trace = False)
     task5_Enco2 = cotask.Task (task5_Encoder2, name = 'Task5_Encoder2', priority = 3, 
                          period = 10, profile = True, trace = False)
     task6_Cont2 = cotask.Task (task6_Controller2, name = 'Task6_Controller2', priority = 2, 
-                         period = 10, profile = True, trace = False)
+                         period = 20, profile = True, trace = False)
     
     cotask.task_list.append (task1_Mot)
     cotask.task_list.append (task2_Enco)
     cotask.task_list.append (task3_Cont)
-    #cotask.task_list.append (task4_Mot2)
-    #cotask.task_list.append (task5_Enco2)
-    #cotask.task_list.append (task6_Cont2)
+    cotask.task_list.append (task4_Mot2)
+    cotask.task_list.append (task5_Enco2)
+    cotask.task_list.append (task6_Cont2)
     
     # Set step response time limit in milliseconds
     stepResponseTimeLimit = 2000
     
+    Contperiod = int(input("Set Controller Period: "))
+    Contperiod2 = int(input("Set Second Controller Period: "))
+    
+    # Set period of controller based on user input
+    task3_Cont.set_period(Contperiod)
+    task6_Cont2.set_period(Contperiod2)
+    
+    # Run the memory garbage collector to ensure memory is as defragmented as
+    # possible before the real-time scheduler is started
+    gc.collect ()
+    
+    
+    #Save start time and flip start flags to true
+    share_StartTime.put(utime.ticks_ms())
+    share_Start.put(1)
+    share_Start_2.put(1)
+    
     while True:
         try:
             
-            Contperiod = int(input("Set Controller Period: "))
-            
-            # Set period of controller based on user input
-            task3_Cont.set_period(Contperiod)
-            
-            # Reset stop flag
-            share_Stop.put(0)
-            
-            # Reset shared variables
-            share_duty_1.put(0)
-            share_setpoint_1.put(math.pi*2)
-            share_pos_1.put(0)
-            share_duty_2.put(0)
-            share_setpoint_2.put(math.pi*2)
-            share_pos_2.put(0)
-            
-            # Run the memory garbage collector to ensure memory is as defragmented as
-            # possible before the real-time scheduler is started
-            gc.collect ()
-
-            # Start a timer to time step response
-            share_StartTime.put(utime.ticks_ms())
-            
-            # Run the scheduler with priority scheduling algorithm
-            while True:
-                cotask.task_list.pri_sched ()
-                
-                if utime.ticks_diff(utime.ticks_ms(), share_StartTime.get()) >= stepResponseTimeLimit:
+            # Check if time limit is met
+            if utime.ticks_diff(utime.ticks_ms(), share_StartTime.get()) >= stepResponseTimeLimit:
                     share_Stop.put(1)
-                    break
+                    share_Stop_2.put(1)
+#                     task3_Cont.schedule()
+#                     task1_Mot.schedule()
+#                     task2_Enco.schedule()
+#                     task6_Cont2.schedule()
+#                     task4_Mot2.schedule()
+#                     task5_Enco2.schedule()
+                    
+            cotask.task_list.rr_sched ()
             
-            utime.sleep_ms(15)
+            # Print (send) data for motor 1
+            if share_Stop.get():
+                while not queue_pos_1.empty():
+                    print(queue_enc1Times.get(), queue_pos_1.get())
+                    
+                print("End of motor 1 data.\n")    
+                share_Stop.put(0)
             
-            task1_Mot.schedule()
-            task2_Enco.schedule()
-            task3_Cont.schedule()
-       
-            while not queue_pos_1.empty():
-                print(queue_enc1Times.get(), queue_pos_1.get())
-            
+            # Print (send) data for motor 2
+            if share_Stop_2.get():
+                while not queue_pos_2.empty():
+                    print(queue_enc2Times.get(), queue_pos_2.get())
+                    
+                share_Stop_2.put(0)
+                share_Start.put(1)
+                share_Start_2.put(1)
+                share_StartTime.put(utime.ticks_ms())
+                
         except KeyboardInterrupt:
             break
         
